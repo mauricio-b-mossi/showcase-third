978f0d1dad2b75c3be9c6a8831afae64c8a459a1
Mauricio Mossi <mauricio.b.mossi@gmail.com>
2021-07-03 20:18:44 -0400

    testing components

---
+++ components/Test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Test = () => {
+    return (
+        <div className="text-xxl font-black text-white">
+            This is just a Test
+        </div>
+    )
+}
+
+export default Test
