5d5b8f52955e0fce542f7a9ed6fa11eba169d700
Mauricio Mossi <mauricio.b.mossi@gmail.com>
2021-07-09 21:24:46 -0400

    fixing overflow

--- pages/aboutDemo.js
+++ pages/aboutDemo.js
@@ -48,7 +48,7 @@ const AboutDemo = ({ about }) => {
                   About
                 </h1>
               </div>
-              <div className="py-12 md:py-24 flex flex-col md:flex-row justify-center items-center text-2xl relative text-white uppercase overflow-x-hidden">
+              <div className="py-12 md:py-24 flex flex-col md:flex-row justify-center items-center text-2xl relative text-white uppercase">
                 <div className="text-sm md:text-xl transform m-4 md:m-0 md:rotate-90 border-b-2 border-t-2   border-white">
                   swipe swipe swipe
                 </div>
