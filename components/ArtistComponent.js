b06bd6870d3470b58b406ce4b79460f17cb30722
Mauricio Mossi <mauricio.b.mossi@gmail.com>
2021-11-18 22:00:41 -0500

    troubleshooting solved

--- components/ArtistComponent.js
+++ components/ArtistComponent.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import SwiperWOLinks from "../../components/SwiperWOLinks";
+import SwiperWOLinks from './SwiperWOLinks'
 
 export default function ArtistComponent({title, description, images}) {
     return (
@@ -7,10 +7,10 @@ export default function ArtistComponent({title, description, images}) {
         <div className="grid md:grid-cols-2 min-h-screen justify-center items-center ">
           <div className="flex flex-col justify-center items-center">
             <h1 className="uppercase text-center text-5xl sm:text-7xl lg:text-8xl 2xl:text-9xl  py-4 md:py-16 font-bold">
-              <div>{props.slugInfo.title}</div>
+              <div>{title}</div>
             </h1>
             <div className="max-w-lg relative transform -translate-y-4 lg:-translate-y-8 2xl:-translate-y-16 text-center py-8 md:py-0 text-xl md:text-2xl">
-              <p>{props.slugInfo.description}</p>
+              <p>{description}</p>
             </div>
           </div>
           <div className="pb-16 md:pb-0">
--- pages/joinUs.js
+++ pages/joinUs.js
@@ -1,7 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Nav from "../components/Nav";
-import Head from "next/head";
-import Slider from "../components/Slider";
 import { sanityClient } from "../sanity";
 import JoinUsTitle from "../components/JoinUsTitle";
 import VideoSectionContainer from "../components/VideoSectionContainer";
